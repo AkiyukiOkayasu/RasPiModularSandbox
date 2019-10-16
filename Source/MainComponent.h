b1a47e6bbd5c73355d073f2612526a6bd94fd6dc
Akiyuki Okayasu <akiyuki@tkrworks.net>
2019-10-16 18:08:56 +0900

    First commit

---
+++ Source/MainComponent.h
@@ -0,0 +1,60 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#if JUCE_LINUX
+#include <pigpio.h>
+#include "AK4558.h"
+#endif
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent : public AudioAppComponent
+{
+public:
+  //==============================================================================
+  MainComponent();
+  ~MainComponent();
+
+  //==============================================================================
+  void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+  void getNextAudioBlock(const AudioSourceChannelInfo &bufferToFill) override;
+  void releaseResources() override;
+
+  //==============================================================================
+  void paint(Graphics &g) override;
+  void resized() override;
+  void showAudioSettings();
+
+private:
+  //==============================================================================
+  // GPIO pin for AK4558 Reset
+  static constexpr unsigned int AK4558RESET = 5;
+
+  //I2C
+  int i2cHandler;
+  static constexpr unsigned int i2cAddr = 0b0010000; //AK4558 I2C address
+  static constexpr unsigned int i2cPort = 1;         //Raspberry Pi I2C Port 1(0 or 1)
+  static constexpr unsigned int i2cFlag = 0;
+
+  //SPI
+  int spiHandler;
+  static constexpr int spiCS = 0; //SPI chip select(0 or 1)
+  static constexpr int spiBaudrate = 200000;
+  static constexpr int spiFlag = 0;
+  std::thread th; //Thread for getting CV value via SPI
+  bool isCVEnabled = true;  
+  std::array<std::atomic<int>, 8> cv;
+
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+};
